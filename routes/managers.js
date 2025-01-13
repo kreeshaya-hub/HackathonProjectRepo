fbf3872725222ece19bed1758c44f045eac2d92a
Tamanna Anand <anandtamanna9@gmail.com>
2025-01-12 20:05:39 -0400

    GET endpoints made

---
+++ routes/managers.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { db } from '../app.js';
+
+//Creating Router for Express to route requests to 
+const router = express.Router()
+router.use(express.json())
+
+router.get('/:id', (req, res) => {
+    try {
+        //prepare statement and annouce it to datebase
+        const statement = db.prepare('SELECT * FROM manager WHERE department_id = ?')
+        //send query to database and execute it 
+        const data = statement.get(req.params.id)
+        if (!data) {
+            return res.status(404).send()
+        }
+        //send data to the client
+        res.send(data)
+
+    } catch (err) {
+        res.status(500).send({ message: 'Try Again Later' })
+    }
+})
+
+
+export default router
